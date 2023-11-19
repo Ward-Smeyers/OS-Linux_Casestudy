8c28c3ff2ab2c5e2d8ad41bbe938ef473488c07e
ward-smeyers <Ward.smeyers@student.thomasmore.be>
2023-11-19 14:16:18 +0100

    basic install and update. (backup dont work)

--- ward_smeyers.sh
+++ ward_smeyers.sh
@@ -9,22 +9,22 @@ else
     while true; do
         read -p "Would you like to create a factorio folder $DIR ? [Y or n]" yn
         case $yn in
-            [Yy]* ) mkdir -pv /opt/Wube-Software/factorio/saves/ /opt/Wube-Software/factorio/mods/; echo "Factorio directory created"; break;;
-            [Nn]* ) exit;;
+            [Yy]* ) mkdir -pv /opt/Wube-Software/factorio/saves/ /opt/Wube-Software/factorio/mods/; echo "Factorio directory created"; break;; # Making the working directory and break out prompt loop
+            [Nn]* ) exit;; # Exit the script
             * ) echo "Please answer yes or no.";;
         esac
     done
 fi
 
-# # Making the working directory
-# mkdir -r /opt/Wube-Software/factorio/saves/ /opt/Wube-Software/factorio/mods/
+# Backup all factorio files
+tar -czf /opt/Wube-Software/factorio/ --directory /opt/Wube-Software/backup-factorio
 
-# # Backup all factorio files
-# tar -czf /opt/Wube-Software/factorio --directory /opt/Wube-Software/backup-factorio
-
-# # Downloading the latest factorio version and saving as factorio_headless.tar.gz
-# wget -O /opt/Wube-Software/factorio_headless.tar.gz https://factorio.com/get-download/stable/headless/linux64
-
-# # unzip and untar the downloaded file 
-# tar -xf /opt/Wube-Software/factorio_headless.tar.gz --directory /opt/Wube-Software
+if [ ! -f "/opt/Wube-Software/factorio_headless.tar.gz" ];
+then
+    # Downloading the latest factorio version and saving as factorio_headless.tar.gz
+    wget -O /opt/Wube-Software/factorio_headless.tar.gz https://factorio.com/get-download/stable/headless/linux64
+fi
+# unzip and untar the downloaded file 
+echo "Unziping /opt/Wube-Software/factorio_headless.tar.gz"
+tar -xf /opt/Wube-Software/factorio_headless.tar.gz --directory /opt/Wube-Software
 
