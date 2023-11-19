f36ec945791a772090ae52d18051d2ba39ad522d
ward-smeyers <Ward.smeyers@student.thomasmore.be>
2023-11-19 11:09:10 +0100

    if test directory

--- ward_smeyers.sh
+++ ward_smeyers.sh
@@ -1,14 +1,22 @@
 #! /bin/bash
 
-# Making the working directory
-mkdir /opt/Wube-Software/factorio/saves/ /opt/Wube-Software/factorio/mods/
 
-# Backup all factorio files
-tar -czf /opt/Wube-Software/factorio --directory /opt/Wube-Software/backup-factorio
+if ls /opt/Wube-Software | grep factorio
+then
+echo "factorio directory found"
+else
+echo "factorio directory missing"
+fi
 
-# Downloading the latest factorio version and saving as factorio_headless.tar.gz
-wget -O /opt/Wube-Software/factorio_headless.tar.gz https://factorio.com/get-download/stable/headless/linux64
+# # Making the working directory
+# mkdir -r /opt/Wube-Software/factorio/saves/ /opt/Wube-Software/factorio/mods/
 
-# unzip and untar the downloaded file 
-tar -xf /opt/Wube-Software/factorio_headless.tar.gz --directory /opt/Wube-Software
+# # Backup all factorio files
+# tar -czf /opt/Wube-Software/factorio --directory /opt/Wube-Software/backup-factorio
+
+# # Downloading the latest factorio version and saving as factorio_headless.tar.gz
+# wget -O /opt/Wube-Software/factorio_headless.tar.gz https://factorio.com/get-download/stable/headless/linux64
+
+# # unzip and untar the downloaded file 
+# tar -xf /opt/Wube-Software/factorio_headless.tar.gz --directory /opt/Wube-Software
 
