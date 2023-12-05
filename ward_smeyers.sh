0ecb3121122752814cf6a4b2af0fb005f7d567be
ward-smeyers <Ward.smeyers@student.thomasmore.be>
2023-12-05 20:08:04 +0100

    Coments and -D option toegevoegt

--- ward_smeyers.sh
+++ ward_smeyers.sh
@@ -7,7 +7,7 @@ then
 else
     echo "Factorio directory missing"
     while true; do
-        read -p "Would you like to create a factorio folder $DIR ? [Y or n]" yn
+        read -p "Would you like to create a factorio folder $DIR? [Y or n]" yn
         case $yn in
             [Yy]* ) mkdir -pv /opt/Wube-Software/factorio/saves/ /opt/Wube-Software/factorio/mods/; echo "Factorio directory created"; break;; # Making the working directory and break out prompt loop
             [Nn]* ) exit;; # Exit the script
@@ -16,10 +16,11 @@ else
     done
 fi
 
-# Backup all factorio files
-tar -czf /opt/Wube-Software/backup-factorio.tar.gz /opt/Wube-Software/factorio/
+# Backup all files in directory /opt/Wube-Software/factorio/ to a targzip file /opt/Wube-Software/backup-factorio.tar.gz
+tar -czf /opt/Wube-Software/backup-factorio.tar.gz $DIR
 
-if [ ! -f "/opt/Wube-Software/factorio_headless.tar.gz" ];
+# Check if latest version of factorio is downloaded
+if [ ! -f "/opt/Wube-Software/factorio_headless.tar.gz" ] | [ $1= '-*[D]*' ];
 then
     # Downloading the latest factorio version and saving as factorio_headless.tar.gz
     wget -O /opt/Wube-Software/factorio_headless.tar.gz https://factorio.com/get-download/stable/headless/linux64
